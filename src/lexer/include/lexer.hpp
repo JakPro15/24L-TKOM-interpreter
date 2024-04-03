1e1bf25321ef4a8b6716c47674728343e58b459e
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2024-04-03 21:10:06 +0200

    Most of (untested) Lexer implementation

--- src/lexer/include/lexer.hpp
+++ src/lexer/include/lexer.hpp
@@ -1,14 +1,42 @@
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
+#include "iErrorHandler.hpp"
 #include "ilexer.hpp"
 #include "ireader.hpp"
 
-class Lexer: ILexer
+#include <functional>
+#include <unordered_map>
+
+class Lexer: public ILexer
 {
 public:
-    Lexer(IReader &reader);
+    Lexer(IReader &reader, IErrorHandler &errorHandler);
     Token getNextToken() override;
+private:
+    static const int MAX_IDENTIFIER_SIZE = 40;
+    static const int MAX_COMMENT_SIZE = 100;
+    static const int MAX_STRING_SIZE = 100;
+
+    IReader &reader;
+    IErrorHandler &errorHandler;
+    // Will also build keywords and bool literals
+    bool buildIdentifier();
+    bool buildComment();
+    bool buildStringLiteral();
+    // Will build int or float literals
+    bool buildNumberLiteral();
+    bool buildOperator();
+    void build2CharOp(wchar_t second, TokenType oneCharType, TokenType twoCharType);
+    void build3CharOp(
+        wchar_t second, wchar_t third, TokenType oneCharType, TokenType twoCharType, TokenType threeCharType
+    );
+    unsigned hexToNumber(wchar_t character);
+    wchar_t hexCharsToNumber(wchar_t firstHex, wchar_t secondHex);
+    void buildEscapeSequence(std::wstringstream &tokenValue);
+    std::unordered_map<wchar_t, std::function<void(void)>> firstCharToFunction;
+    void prepareOperatorMap();
+    Token tokenBuilt;
 };
 
 #endif
--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -2,6 +2,8 @@
 
 #include "token.hpp"
 
+#include <format>
+#include <sstream>
 #include <string>
 #include <unordered_map>
 
@@ -27,9 +29,230 @@ const std::unordered_map<std::wstring, TokenType> keywordToTokenType = {
     {L"float", TokenType::KW_FLOAT},
     {L"bool", TokenType::KW_BOOL},
     {L"str", TokenType::KW_STR},
+    {L"true", TokenType::TRUE_LITERAL},
+    {L"false", TokenType::FALSE_LITERAL},
 };
+
+Lexer::Lexer(IReader &reader, IErrorHandler &errorHandler): reader(reader), errorHandler(errorHandler)
+{
+    prepareOperatorMap();
+}
+
+bool Lexer::buildIdentifier()
+{
+    if(!(std::iswalpha(reader.get()) || reader.get() == L'_'))
+        return false;
+
+    std::wstringstream tokenValue;
+    tokenValue.put(reader.get());
+    reader.next();
+
+    while(std::isalnum(reader.get()) || reader.get() == L'_' || reader.get() == L'\'')
+    {
+        tokenValue.put(reader.get());
+        if(tokenValue.tellg() > MAX_IDENTIFIER_SIZE)
+            errorHandler.handleError(
+                Error::LEXER_IDENTIFIER_TOO_LONG, L"Maximum identifier size exceeded", tokenBuilt.position
+            );
+        reader.next();
+    }
+    std::wstring result = tokenValue.str();
+    if(keywordToTokenType.find(result) != keywordToTokenType.end())
+        tokenBuilt.type = keywordToTokenType.at(result);
+    else
+    {
+        tokenBuilt.type = TokenType::IDENTIFIER;
+        tokenBuilt.value = result;
+    }
+    return true;
+}
+
+bool Lexer::buildComment()
+{
+    if(reader.get() != L'#')
+        return false;
+
+    reader.next();
+
+    std::wstringstream tokenValue;
+    while(reader.get() != L'\n')
+    {
+        tokenValue.put(reader.get());
+        if(tokenValue.tellg() > MAX_COMMENT_SIZE)
+            errorHandler.handleError(
+                Error::LEXER_COMMENT_TOO_LONG, L"Maximum comment size exceeded", tokenBuilt.position
+            );
+        reader.next();
+    }
+    tokenBuilt.type = TokenType::COMMENT;
+    tokenBuilt.value = tokenValue.str();
+    return true;
+}
+
+unsigned Lexer::hexToNumber(wchar_t character)
+{
+    if(character >= L'0' && character <= L'9')
+        return character - L'0';
+    if(character >= L'a' && character <= L'f')
+        return character - L'a' + 10;
+    if(character >= L'A' && character <= L'F')
+        return character - L'A' + 10;
+    errorHandler.handleError(
+        Error::LEXER_INVALID_HEX_CHAR, std::format(L"The character {} is not a valid hex digit", character),
+        tokenBuilt.position
+    );
+}
+
+wchar_t Lexer::hexCharsToNumber(wchar_t firstHex, wchar_t secondHex)
+{
+    unsigned first = hexToNumber(firstHex);
+    unsigned second = hexToNumber(secondHex);
+    return static_cast<wchar_t>(first * 16 + second);
+}
+
+void Lexer::buildEscapeSequence(std::wstringstream &tokenValue)
+{
+    switch(reader.get())
+    {
+    case L't':
+        tokenValue.put(L'\t');
+        break;
+    case L'r':
+        tokenValue.put(L'\r');
+        break;
+    case L'n':
+        tokenValue.put(L'\n');
+        break;
+    case L'"':
+        tokenValue.put(L'"');
+        break;
+    case L'\\':
+        tokenValue.put(L'\\');
+        break;
+    case L'x':
+        reader.next();
+        wchar_t firstHex = reader.get();
+        reader.next();
+        wchar_t secondHex = reader.get();
+        tokenValue.put(hexCharsToNumber(firstHex, secondHex));
+        break;
+    default:
+        errorHandler.handleError(
+            Error::LEXER_UNKNOWN_ESCAPE, std::format(L"\\{} is not a valid escape sequence", reader.get()),
+            tokenBuilt.position
+        );
+    }
+}
+
+bool Lexer::buildStringLiteral()
+{
+    if(reader.get() != L'"')
+        return false;
+    reader.next();
+
+    std::wstringstream tokenValue;
+    while(reader.get() != L'"')
+    {
+        if(reader.get() == L'\n')
+            errorHandler.handleError(
+                Error::LEXER_NEWLINE_IN_STRING, L"Newline character in string literal encountered", tokenBuilt.position
+            );
+        else if(reader.get() == L'\\')
+        {
+            reader.next();
+            buildEscapeSequence(tokenValue);
+        }
+        else
+        {
+            tokenValue.put(reader.get());
+            if(tokenValue.tellg() > MAX_STRING_SIZE)
+                errorHandler.handleError(
+                    Error::LEXER_STRING_TOO_LONG, L"Maximum string literal size exceeded", tokenBuilt.position
+                );
+        }
+        reader.next();
+    }
+    reader.next();
+
+    tokenBuilt.type = TokenType::STR_LITERAL;
+    tokenBuilt.value = tokenValue.str();
+    return true;
+}
+
+void Lexer::prepareOperatorMap()
+{
+    using enum TokenType;
+    firstCharToFunction.emplace(L'{', [&]() { tokenBuilt.type = LBRACE; });
+    firstCharToFunction.emplace(L'}', [&]() { tokenBuilt.type = RBRACE; });
+    firstCharToFunction.emplace(L';', [&]() { tokenBuilt.type = SEMICOLON; });
+    firstCharToFunction.emplace(L'(', [&]() { tokenBuilt.type = LPAREN; });
+    firstCharToFunction.emplace(L')', [&]() { tokenBuilt.type = RPAREN; });
+    firstCharToFunction.emplace(L'-', [&]() { build2CharOp(L'>', OP_MINUS, ARROW); });
+    firstCharToFunction.emplace(L',', [&]() { tokenBuilt.type = COMMA; });
+    firstCharToFunction.emplace(L'$', [&]() { tokenBuilt.type = DOLLAR_SIGN; });
+    firstCharToFunction.emplace(L'=', [&]() { build3CharOp(L'=', L'=', OP_ASSIGN, OP_EQUAL, OP_IDENTICAL); });
+    firstCharToFunction.emplace(L'!', [&]() { build3CharOp(L'=', L'=', OP_CONCAT, OP_NOT_EQUAL, OP_NOT_IDENTICAL); });
+    firstCharToFunction.emplace(L'.', [&]() { tokenBuilt.type = OP_DOT; });
+    firstCharToFunction.emplace(L'@', [&]() { tokenBuilt.type = OP_STR_MULTIPLY; });
+    firstCharToFunction.emplace(L'>', [&]() { build2CharOp(L'=', OP_GREATER, OP_GREATER_EQUAL); });
+    firstCharToFunction.emplace(L'<', [&]() { build2CharOp(L'=', OP_LESSER, OP_LESSER_EQUAL); });
+    firstCharToFunction.emplace(L'+', [&]() { tokenBuilt.type = OP_PLUS; });
+    firstCharToFunction.emplace(L'*', [&]() { build2CharOp(L'*', OP_MULTIPLY, OP_EXPONENT); });
+    firstCharToFunction.emplace(L'/', [&]() { build2CharOp(L'/', OP_DIVIDE, OP_FLOOR_DIVIDE); });
+    firstCharToFunction.emplace(L'%', [&]() { tokenBuilt.type = OP_MODULO; });
+    firstCharToFunction.emplace(L'[', [&]() { tokenBuilt.type = LSQUAREBRACE; });
+    firstCharToFunction.emplace(L']', [&]() { tokenBuilt.type = RSQUAREBRACE; });
+}
+
+void Lexer::build2CharOp(wchar_t second, TokenType oneCharType, TokenType twoCharType)
+{
+    if(reader.get() == second)
+    {
+        tokenBuilt.type = twoCharType;
+        reader.next();
+    }
+    else
+        tokenBuilt.type = oneCharType;
+}
+
+void Lexer::build3CharOp(
+    wchar_t second, wchar_t third, TokenType oneCharType, TokenType twoCharType, TokenType threeCharType
+)
+{
+    if(reader.get() == second)
+    {
+        reader.next();
+        if(reader.get() == third)
+        {
+            tokenBuilt.type = threeCharType;
+            reader.next();
+        }
+        else
+            tokenBuilt.type = twoCharType;
+    }
+    else
+        tokenBuilt.type = oneCharType;
+}
+
+bool Lexer::buildOperator()
+{
+    wchar_t firstChar = reader.get();
+    if(firstCharToFunction.find(firstChar) == firstCharToFunction.end())
+        return false;
+    reader.next();
+    firstCharToFunction.at(firstChar)();
+    return true;
+}
 
 Token Lexer::getNextToken()
 {
-    return Token();
+    // skip whitespace
+    tokenBuilt.position = reader.getPosition();
+    if(reader.get() == IReader::EOT)
+    {
+        tokenBuilt.type = TokenType::EOT;
+        return tokenBuilt;
+    }
+    // try build things
+    return this->tokenBuilt;
 }

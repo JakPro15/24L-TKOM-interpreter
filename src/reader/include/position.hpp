dbd0bd657e1acfaee198861b08b5b7c684ea3e01
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2024-04-20 11:54:03 +0200

    Made comparison operators of Position default

--- src/reader/include/position.hpp
+++ src/reader/include/position.hpp
@@ -1,13 +1,15 @@
 #ifndef POSITION_HPP
 #define POSITION_HPP
 
+#include <compare>
+
 struct Position
 {
     unsigned line, column;
     Position() = default;
     Position(const Position &) = default;
     Position(unsigned line, unsigned column);
-    bool operator==(const Position &other) const;
+    auto operator<=>(const Position &other) const = default;
 };
 
 #endif
--- src/reader/position.cpp
+++ src/reader/position.cpp
@@ -1,8 +1,3 @@
 #include "position.hpp"
 
 Position::Position(unsigned line, unsigned column): line(line), column(column) {}
-
-bool Position::operator==(const Position &other) const
-{
-    return this->line == other.line && this->column == other.column;
-}

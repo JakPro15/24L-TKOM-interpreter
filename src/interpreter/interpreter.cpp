e4106e18cfee4ac29b25443d229e56def0746422
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2024-05-27 16:45:02 +0200

    Made Interpreter take parseFromFile function as argument and implement DocumentTreeVisitor

--- src/interpreter/interpreter.cpp
+++ src/interpreter/interpreter.cpp
@@ -1,5 +1,10 @@
 #include "interpreter.hpp"
 
-Interpreter::Interpreter(std::vector<std::wstring> arguments, std::istream &input, std::ostream &output):
-    arguments(arguments), input(input), output(output)
+Interpreter::Interpreter(
+    std::vector<std::wstring> arguments, std::istream &input, std::ostream &output,
+    std::function<Program(std::wifstream &, std::wstring)> parseFromFile
+): arguments(arguments), input(input), output(output), parseFromFile(parseFromFile)
 {}
+
+#define EMPTY_VISIT(type) \
+    void Interpreter::visit(type &visited) {}
